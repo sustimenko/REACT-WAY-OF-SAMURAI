d9511e0d18b3aff8fa738853dbcf6b2ba5b25b68
SUN <sergey.ustimenko1974@gmail.com>
2022-05-10 01:45:35 +0300

    initial

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://variety.com/wp-content/uploads/2022/03/MCDTRAG_EC024.jpg?w=681&h=383&crop=1' />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>
+    </div>);
+}
+
+export default Profile;
