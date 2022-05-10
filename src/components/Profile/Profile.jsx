8859936598070a71e86521559bccb3dab152a560
SUN <sergey.ustimenko1974@gmail.com>
2022-05-10 21:57:23 +0300

    18. Уроки React JS (страница Dialogs) - react курсы бесплатно

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,16 +3,16 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts.jsx'
 
 const Profile = () => {
-  return (
-    <div className={s.content}>
-      <div>
-        <img src='https://variety.com/wp-content/uploads/2022/03/MCDTRAG_EC024.jpg?w=681&h=383&crop=1' />
-      </div>
-      <div>
-        ava + description
-      </div>
-      <MyPosts />
-    </div>);
+    return (
+        <div>
+            <div>
+                <img src='https://variety.com/wp-content/uploads/2022/03/MCDTRAG_EC024.jpg?w=681&h=383&crop=1'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
+        </div>);
 }
 
 export default Profile;
