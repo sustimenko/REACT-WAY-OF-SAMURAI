fa50ef4f2816ba5841f46cd000c43101f2e1ebf4
SUN <sergey.ustimenko1974@gmail.com>
2022-05-10 12:06:52 +0300

    14- css-модули, css-modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <div>
         <img src='https://variety.com/wp-content/uploads/2022/03/MCDTRAG_EC024.jpg?w=681&h=383&crop=1' />
       </div>
@@ -14,11 +15,11 @@ const Profile = () => {
         <div>
           New post
         </div>
-        <div>
-          <div>
+        <div className={s.posts}>
+          <div className={s.item}>
             Post 1
           </div>
-          <div>
+          <div className={s.item}>
             Post 2
           </div>
         </div>
