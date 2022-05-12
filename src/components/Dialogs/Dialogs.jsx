79313ebb0b723aa5346977597fe464a24b1ed5e4
SUN <sergey.ustimenko1974@gmail.com>
2022-05-12 03:11:39 +0300

    21. Уроки React JS (Верстаем страницу Диалогов) - react курсы бесплатно

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 
+
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialog+' ' + s.active}>
+                <NavLink to="dialogs/1"> Dimuch</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to="dialogs/2">Andrey</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to="dialogs/3">Sveta</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to="dialogs/4">Sasha</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to="dialogs/5">Dimuch</NavLink>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi </div>
+                <div className={s.dialog}>How is your IT-KAMASUTRA</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
+
+        </div>
     )
 }
 
